1fa77fd6ca28ef7d6a42d87f828e93e5b696bde7
baiyinnamula <byn@netsun.com>
2025-03-11 13:51:56 +0800

    init project

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+process.env.Vue_APP_VERSION = require('./package.json').version
+process.env.Vue_APP_NAME = require('./package.json').name
 module.exports = defineConfig({
   transpileDependencies: true
 })
